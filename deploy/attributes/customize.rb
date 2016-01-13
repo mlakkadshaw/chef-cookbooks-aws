e7816fb8565eb15eb98213d3e80d2edd1a2b6ddf
Mohammed Lakkadshaw <mohammed.lakkadshaw@gmail.com>
2016-01-13 11:27:32 +0530

    added line to install forever

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -1,3 +1,3 @@
   # mqtt
-  default[:deploy][application][:mqtt][:restart_command] = "forever stopall; forever start #{default[:deploy][application][:deploy_to}/current/index.js"
+  default[:deploy][application][:mqtt][:restart_command] = "npm install -g forever; forever stopall; forever start #{default[:deploy][application][:deploy_to}/current/index.js"
   default[:deploy][application][:mqtt][:stop_command] = "forever stopall"
