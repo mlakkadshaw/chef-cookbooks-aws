bda641da4b0e65b452e7d303fadadc66d5de7796
Mohammed Lakkadshaw <mohammed.lakkadshaw@gmail.com>
2016-01-13 15:27:59 +0530

    updated customize.rb

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -1,5 +1,5 @@
   # mqtt
   node[:deploy].each do |application, deploy|
-  	node[:deploy][application][:mqtt][:restart_command] = "npm install -g forever; forever stopall; forever start #{default[:deploy][application][:deploy_to]}/current/index.js"
-  	node[:deploy][application][:mqtt][:stop_command] = "forever stopall"
+  	normal[:deploy][application][:mqtt][:restart_command] = "npm install -g forever; forever stopall; forever start #{default[:deploy][application][:deploy_to]}/current/index.js"
+  	normal[:deploy][application][:mqtt][:stop_command] = "forever stopall"
   end
