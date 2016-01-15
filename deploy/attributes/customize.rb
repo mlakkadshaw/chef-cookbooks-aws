0c75dd2bac225342d56e112e7226ae6742078e29
Mohammed Lakkadshaw <mohammed.lakkadshaw@gmail.com>
2016-01-15 10:29:44 +0530

    updated the customize rb to check if app is mqtt

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -1,6 +1,6 @@
   # mqtt
   node[:deploy].each do |application, deploy|
-  	if deploy[application] == 'mqtt'
+  	if application == 'mqtt'
   		normal[:deploy][application][:mqtt][:restart_command] = "npm install -g forever; forever stopall; forever start #{normal[:deploy][application][:deploy_to]}/current/index.js"
   		normal[:deploy][application][:mqtt][:stop_command] = "forever stopall"
   	end
